12b892766a8f8f1b0ec911bfed2d4fd0abb3c17e
Fadi Asaad <firon1222@gmail.com>
2023-08-26 01:35:19 +0300

    add task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create file at /tmp with content
+
+file { 'Create File':
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
