1b75d56986616630c8b2c701c3476db5344f789a
Fadi Asaad <firon1222@gmail.com>
2023-08-29 21:40:24 +0300

    add task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,38 @@
+# Setup Ubuntu server with NGINX Web Server
+
+exec { 'apt update':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n\'est pas une page",
+}
+
+exec { 'configure nginx redirection':
+  command  => 'sed -i "42 a\ \tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n" /etc/nginx/sites-enabled/default',
+  user     => 'root',
+  provider => 'shell',
+}
+
+exec { 'configure custom 404 error page':
+  command  => 'sed -i "42 a\ \terror_page 404 /404.html;\n" /etc/nginx/sites-enabled/default',
+  user     => 'root',
+  provider => 'shell',
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+}
