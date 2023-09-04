f187f37b817337bd3c49bc92cb8ab2e66f72cac2
Fadi Asaad <firon1222@gmail.com>
2023-09-05 00:29:27 +0300

    Revert "update task 2"
    This reverts commit 102c104ba61da58f6fbec116eddc491bc47a980a.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,6 +10,28 @@ package { 'nginx':
   ensure => installed,
 }
 
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'present',
+  content => "Hello World!\n",
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n\'est pas une page\n",
+}
+
+exec { 'configure nginx redirection':
+  command  => 'sed -i "42 a\ \tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n" /etc/nginx/sites-enabled/default',
+  user     => 'root',
+  provider => 'shell',
+}
+
+exec { 'configure custom 404 error page':
+  command  => 'sed -i "42 a\ \terror_page 404 /404.html;\n" /etc/nginx/sites-enabled/default',
+  user     => 'root',
+  provider => 'shell',
+}
+
 exec { 'add X-Served-By header':
   command  => 'sed -i "42 a\ \tadd_header X-Served-By \$hostname;\n" /etc/nginx/sites-enabled/default',
   user     => 'root',
