09d4378dd3b28c74883b541000ac379391e89167
Fadi Asaad <firon1222@gmail.com>
2023-10-11 01:13:05 +0300

    fix: task 1: update endpoint and add my username

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,14 +9,11 @@ def top_ten(subreddit):
     Args:
         subreddit (str): the subreddit name"""
     res = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/top.json?limit=10",
-        headers={'User-Agent': 'alx:dev.davenchy:v1.0'},
+        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+        headers={'User-Agent': 'alx:dev.davenchy:v1.0 (by /u/davenchy)'},
         allow_redirects=False,
     )
     if not res.ok:
         print("None")
         return
-
-    children = res.json()['data']['children']
-    for child in children:
-        print(child['data']['title'])
+    [print(c['data']['title']) for c in res.json()['data']['children']]
