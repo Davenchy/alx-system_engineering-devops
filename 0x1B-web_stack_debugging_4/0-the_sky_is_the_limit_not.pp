440676088f75ea108850806d369e25b0e3851afd
Fadi Asaad <firon1222@gmail.com>
2023-10-21 02:33:43 +0300

    add task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+file_path = '/etc/default/nginx'
+$line = 'ULIMIT="-n 4096"'
+
+exec { 'modify the nginx limit':
+        provider => 'shell',
+        command  => "sed -i 's/^ULIMIT=.*/${line}/' ${file_path}",
+        unless   => "grep -q '^${line}\$' ${file_path}",
+}
+
+exec { 'restart nginx server':
+        provider => 'shell',
+        command  => 'sudo service nginx restart',
+}
